88502016344ed4b9e4d53f5b0811530264d45f41
maniaug81 <maniaug81@gmail.com>
2022-06-30 16:01:43 +0530

    Update variables.tf
    updating variables

--- variables.tf
+++ variables.tf
@@ -1,45 +1,45 @@
 variable resource_group {
   description = "Name of resource group to provision resources"
-  default     = "270004UG0W-icct-vpc-schematics"
+  default     = "674000MFBA-icct-vpc-schematics"
 }
 
 variable "ibmcloud_region" {
   description = "Preferred IBM Cloud region to use for your infrastructure"
-  default = "us-south"
+  default = "eu-de"
 }
 
 variable "vpc_name" {
-  default = "icct-lab-vpc"
+  default = "manish-vpc-frankfurt"
   description = "Name of your VPC"
 }
 
 variable "zone1" {
-  default = "us-south-1"
+  default = "eu-de-1"
   description = "Define the 1st zone of the region"
 }
 
 variable "zone2" {
-  default = "us-south-2"
+  default = "eu-de-2"
   description = "Define the 2nd zone of the region"
 }
 
 variable "zone1_cidr" {
-  default = "172.16.1.0/24"
+  default = "10.16.1.0/24"
   description = "CIDR block to be used for zone 1"
 }
 
 variable "zone2_cidr" {
-  default = "172.16.2.0/24"
+  default = "10.16.2.0/24"
   description = "CIDR block to be used for zone 2"
 }
 
 variable "ssh_key_name" {
-  default = "YOU MUST CREATE A VPC KEY"
+  default = "frankfurt-key"
   description = "Name of existing VPC SSH Key"
 }
 
 variable "image" {
-  default = "r018-b30c1f1a-b38d-42dc-85b3-f79c34b0d2c3"
+  default = "r010-4ebba778-f642-4dfd-85bd-3aa53a20fd95"
   description = "OS Image ID to be used for virtual instances"
 }
 
